f45ccb6c40dca63f1280b65f4df79019aee99a69
Simon Phillips <simon.phillips01@icloud.com>
2019-09-18 22:28:49 +0100

    Passing redux state via props

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -3,10 +3,14 @@ import firebase from '../../firebase';
 import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
 
 class UserPanel extends React.Component {
+    state = {
+        user: this.props.currentUser
+    }
+
     dropDownOptions = () => [
         {
             key: 'user',
-            text: <span>Signed in as <strong>User</strong></span>,
+            text: <span>Signed in as <strong>{this.state.user.displayName}</strong></span>,
             disabled: true
         },
         {
@@ -41,9 +45,9 @@ class UserPanel extends React.Component {
 
                     {/* User Dropdown */}
                     <Header style={{ padding: '0.25em' }} as="h4" inverted>
-                        <Dropdown trigger={
-                            <span>User</span>
-                        } options={this.dropDownOptions()} />
+                        <Dropdown 
+                        trigger={<span>{this.state.user.displayName}</span>} 
+                        options={this.dropDownOptions()} />
                     </Header>
                 </Grid.Column>
             </Grid>
