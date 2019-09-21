74d9bd2330cb44f05fd4ca3150fe38ba8c73ca9a
Simon Phillips <simon.phillips01@icloud.com>
2019-09-21 16:03:36 +0100

    Put media file in state, Added mimetype validation

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -60,6 +60,10 @@ class MessageForm extends React.Component {
             })
         }
     }
+
+    uploadFile = (file, metadata) => {
+        console.log(file, metadata);
+    }
     
     render() {
         const { errors, message, loading, modal } = this.state;
@@ -100,6 +104,7 @@ class MessageForm extends React.Component {
                     <FileModal 
                         modal={modal}
                         closeModal={this.closeModal}
+                        uploadFile={this.uploadFile}
                     />
                 </Button.Group>
             </Segment>
