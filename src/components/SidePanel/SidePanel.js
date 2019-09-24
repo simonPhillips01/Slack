588b1a6cb22d90bdafea3d11134f14b345bc8d9d
Simon Phillips <simon.phillips01@icloud.com>
2019-09-24 17:00:10 +0100

    Created direct messages component

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Menu } from 'semantic-ui-react';
+
 import UserPanel from './UserPanel';
 import Channels from './Channels';
-import { Menu } from 'semantic-ui-react';
+import DirectMessages from './DirectMessages';
 
 class SidePanel extends React.Component {
     render() {
@@ -16,7 +18,8 @@ class SidePanel extends React.Component {
                 style={{ background: '#4c3c4c', fontSize: '1.2rem'}}
             >
                 <UserPanel currentUser={ currentUser }/> 
-                <Channels currentUser={ currentUser }/>   
+                <Channels currentUser={ currentUser }/>
+                <DirectMessages />  
             </Menu>
         )
     }
