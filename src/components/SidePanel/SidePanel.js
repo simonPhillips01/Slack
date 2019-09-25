4ce0e0fefbbc0f3213cd8d108b51eefcd886f4f5
Simon Phillips <simon.phillips01@icloud.com>
2019-09-25 23:03:52 +0100

    Added users to direct messages component, show if online/offline

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -19,9 +19,9 @@ class SidePanel extends React.Component {
             >
                 <UserPanel currentUser={ currentUser }/> 
                 <Channels currentUser={ currentUser }/>
-                <DirectMessages />  
+                <DirectMessages currentUser={ currentUser }/>  
             </Menu>
-        )
+        );
     }
 }
 
