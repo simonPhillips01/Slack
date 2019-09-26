49b06f82029fec0aa835ae25fdac9c6bfe652ae9
Simon Phillips <simon.phillips01@icloud.com>
2019-09-26 14:40:19 +0100

    Added starred component to hold starred (favourited) channels

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -4,6 +4,7 @@ import { Menu } from 'semantic-ui-react';
 import UserPanel from './UserPanel';
 import Channels from './Channels';
 import DirectMessages from './DirectMessages';
+import Starred from './Starred';
 
 class SidePanel extends React.Component {
     render() {
@@ -17,7 +18,8 @@ class SidePanel extends React.Component {
                 vertical
                 style={{ background: '#4c3c4c', fontSize: '1.2rem'}}
             >
-                <UserPanel currentUser={ currentUser }/> 
+                <UserPanel currentUser={ currentUser }/>
+                <Starred />
                 <Channels currentUser={ currentUser }/>
                 <DirectMessages currentUser={ currentUser }/>  
             </Menu>
