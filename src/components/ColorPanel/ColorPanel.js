e43c54af5971133cc409367d062edf0f77412a07
Simon Phillips <simon.phillips01@icloud.com>
2019-09-29 17:06:38 +0100

    Added color picker modal to color panel

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import { Sidebar, Menu, Divider, Button } from 'semantic-ui-react';
+import { Sidebar, Menu, Divider, Button, Modal, Icon, Label } from 'semantic-ui-react';
+import { SliderPicker } from 'react-color';
 
 class ColorPanel extends React.Component {
+    state = {
+        modal: false
+    }
+
+    openModel = () => this.setState({ modal: true });
+
+    closeModel = () => this.setState({ modal: false });
+    
     render() {
+        const { modal } = this.state;
+
         return (
             <Sidebar 
                 as={Menu}
@@ -13,7 +24,26 @@ class ColorPanel extends React.Component {
                 width="very thin"
             >
                 <Divider />
-                <Button icon="add" size="small" color="blue" />
+                <Button icon="add" size="small" color="blue" onClick={this.openModal} />
+
+                {/* Color Picker Modal */}
+                <Modal basic open={modal} onClose={this.closeModal}>
+                    <Modal.Header>Choose App Colours</Modal.Header>
+                    <Modal.Content>
+                        <Label content="Primary Colour" />
+                        <SliderPicker />
+                        <Label content="Secondary Colour" />
+                        <SliderPicker />
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button color="green" inverted>
+                            <Icon name="checkmark" /> Save Colours
+                        </Button>
+                        <Button color="red" inverted onClick={this.closeModal}>
+                            <Icon name="remove" /> Cancel
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
             </Sidebar>
         );
     }
