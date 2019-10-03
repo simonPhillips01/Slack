962aa9e18a84b8a604ff8fac087a3ce06298e66c
Simon Phillips <simon.phillips01@icloud.com>
2019-10-03 13:45:23 +0100

    Automatic scroll to bottom upon new message

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -38,6 +38,16 @@ class Messages extends React.Component {
         }
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (this.messagesEnd) {
+            this.scrollToBottom();
+        }
+    }
+
+    scrollToBottom = () => {
+        this.messagesEnd.scrollIntoView({ behavior: 'smooth' });
+    }
+
     addListeners = channelId => {
         this.addMessageListener(channelId);
         this.addTypingListeners(channelId);
@@ -233,6 +243,7 @@ class Messages extends React.Component {
                         {searchTerm ? this.displayMessages(searchResults) : 
                         this.displayMessages(messages)}
                         {this.displayTypingUsers(typingUsers)}
+                        <div ref={node => (this.messagesEnd = node)}></div>
                     </Comment.Group>
                 </Segment>
 
