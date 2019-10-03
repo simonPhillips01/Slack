4de1bc0d997cf05fc6827f16e08562fcf826180b
Simon Phillips <simon.phillips01@icloud.com>
2019-10-03 14:19:52 +0100

    Show message skeleton when messages loading

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -8,6 +8,7 @@ import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
 import Message from './Message';
 import Typing from './Typing';
+import Skeleton from './Skeleton';
 
 class Messages extends React.Component {
     state = {
@@ -221,10 +222,19 @@ class Messages extends React.Component {
             </div>
         ))
     )
+
+    displayMessagesSkeleton = loading => 
+            loading ? (
+                <React.Fragment>
+                    {[...Array(10)].map((_, i) => (
+                        <Skeleton key={i} />
+                    ))}
+                </React.Fragment>
+            ) : null;
     
     render() {
         // prettier-ignore
-        const { messagesRef, messages, channel, user, numUniqueUsers, searchTerm, searchResults, searchLoading, privateChannel, isChannelStarred, typingUsers } = this.state;
+        const { messagesRef, messages, channel, user, numUniqueUsers, searchTerm, searchResults, searchLoading, privateChannel, isChannelStarred, typingUsers, messagesLoading } = this.state;
 
         return (
             <React.Fragment>
@@ -240,6 +250,7 @@ class Messages extends React.Component {
 
                 <Segment>
                     <Comment.Group className="messages">
+                        {this.displayMessagesSkeleton(messagesLoading)}
                         {searchTerm ? this.displayMessages(searchResults) : 
                         this.displayMessages(messages)}
                         {this.displayTypingUsers(typingUsers)}
